6cca2a980db63da7931e844dd25ce2f4211b2aa9
leroxi <lenarpavlenko@gmail.com>
2024-08-04 19:51:18 +0300

    final real final reall

--- src/main/java/ru/myproj/projectlenar/model/Client.java
+++ src/main/java/ru/myproj/projectlenar/model/Client.java
@@ -5,10 +5,10 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
-@EqualsAndHashCode
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @Table(name = "CLIENTS")
 public class Client {
     @Id
@@ -24,45 +24,6 @@ public class Client {
     private LocalDate birthday;
     @Column(name = "message_send")
     private boolean messageSend;
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public boolean isMessageSend() {
-        return messageSend;
-    }
-
-    public void setMessageSend(boolean messageSend) {
-        this.messageSend = messageSend;
-    }
 }
 
 
