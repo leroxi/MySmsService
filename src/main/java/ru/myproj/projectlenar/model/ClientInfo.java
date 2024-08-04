ed11b4978aed850a2970911c552eef3edfe38938
Ленар <160050788+leroxi@users.noreply.github.com>
2024-08-04 19:54:46 +0300

    Update ClientInfo.java

--- src/main/java/ru/myproj/projectlenar/model/ClientInfo.java
+++ src/main/java/ru/myproj/projectlenar/model/ClientInfo.java
@@ -2,15 +2,15 @@ package ru.myproj.projectlenar.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@EqualsAndHashCode
 @Schema(description = "ДТО клиента")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class ClientInfo {
     @Schema(description = "Индефикатор нашего клиента")
     private Integer id;
@@ -22,45 +22,5 @@ public class ClientInfo {
     private LocalDate birthday;
     @Schema(description = "Статус отправленного сообщения клиенту")
     private boolean messageSend;
-
-    // todo аннотация Data
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public boolean isMessageSend() {
-        return messageSend;
-    }
-
-    public void setMessageSend(boolean messageSend) {
-        this.messageSend = messageSend;
-    }
 }
+
